c91570b9f1ad4044e980219285df67886373730b
Yuhui Seo <seoyh1104@naver.com>
2022-07-12 15:05:29 +0900

    Add Dark Theme Toggle Button

--- assets/js/custom/dark-theme.js
+++ assets/js/custom/dark-theme.js
@@ -5,6 +5,12 @@ const darkTheme = [...document.styleSheets].find(style => /(main_dark.css)$/.tes
 let setDarkMode = (isDark) => {
     darkTheme.disabled = isDark !== true;
     defaultTheme.disabled = isDark === true;
+    
+    if (window['customUtterances']) {
+        const customUtterances = window['customUtterances'];
+        customUtterances.onChange(isDark ? customUtterances.darkTheme : customUtterances.theme);
+    }
+    
     localStorage.setItem('theme', isDark ? 'dark' : 'default');
 }
 
